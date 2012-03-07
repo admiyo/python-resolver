69f318028f8e200df712f7b3de5ac6803fe1399a
Adam Young <ayoung@redhat.com>
2012-03-06 21:14:53 -0500

    pep8

--- src/resolver/__init__.py
+++ src/resolver/__init__.py
@@ -13,11 +13,12 @@ class Scope(object):
 scope_map = dict([])
 
 
-def register(classname, proxy, scope = None):
-    if (scope_map.has_key(scope)):
+def register(classname, proxy, scope=None):
+    if (scope in scope_map):
         proxy_map = scope_map[scope]
-        if (proxy_map.has_key(classname)):
-            raise ValueError(classname + "already has creation proxy registered")
+        if (classname in proxy_map):
+            raise ValueError(classname +
+                            "already has creation proxy registered")
     else:
         proxy_map = dict()
         scope_map[scope] = proxy_map
@@ -29,18 +30,17 @@ class Resolver(object):
     classdocs
     '''
     instances = dict()
-    
 
-    def __init__(self,scope = None):
+    def __init__(self, scope=None):
         '''
         Constructor
         '''
         self.proxies = scope_map[scope]
-        
+
     def resolve(self, classname):
-        if self.instances.has_key(classname):
+        if (classname in self.instances):
             return  self.instances[classname]
-        if self.proxies.has_key(classname):
+        if classname in self.proxies:
             inst = self.proxies[classname](self)
             self.instances[classname] = inst
             return inst
--- src/test/resolver_test.py
+++ src/test/resolver_test.py
@@ -7,47 +7,49 @@ import unittest
 import resolver
 import test
 
+
 class SampleBase:
     message = "Base"
+
     def operation(self):
         return self.message
 
+
 class SampleSub(SampleBase):
     message = "sub"
+
 
 def create_base(resolver):
     return SampleSub()
 
 
 class SampleOp:
-    def __init__(self,base):
+
+    def __init__(self, base):
         self.message = "something"
         self.base = base
+
     def operation(self):
         return self.base.operation()
 
 
 def do_something(resolver):
     return  SampleOp(resolver.resolve(SampleBase))
-        
+
 
 class Test(unittest.TestCase):
-
 
     def setUp(self):
         pass
 
-
     def tearDown(self):
         pass
-
 
     def testName(self):
         resolver.register(SampleOp, do_something)
         resolver.register(SampleBase, create_base)
-        
-        res =   resolver.Resolver()  
-        
+
+        res = resolver.Resolver()
         op = res.resolve(SampleOp)
         self.assertEqual("sub", op.operation(), "factory got right instance")
 
