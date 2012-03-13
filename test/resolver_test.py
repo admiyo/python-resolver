fa520b287afc545e679e75a985cf10509d963854
Adam Young <ayoung@redhat.com>
2012-03-13 13:19:46 -0400

    Resolution chaining

--- test/resolver_test.py
+++ test/resolver_test.py
@@ -38,17 +38,32 @@ def do_something(resolver):
     return  SampleOp(resolver.resolve(SampleBase))
 
 
-class Test(unittest.TestCase):
+class RequestObject:
+    def __init__(self, global_object):
+        self.global_object = global_object
 
+    def do_op(self):
+        return self.global_object.operation()
+
+
+def request_object_factory(resolver):
+    global_object = resolver.resolve(SampleBase)
+    return RequestObject(global_object)
+
+
+class Test(unittest.TestCase):
     def setUp(self):
         resolver.register(SampleOp, do_something)
         resolver.register(SampleBase, create_base)
+        resolver.register(RequestObject, request_object_factory,
+                          resolver.REQUEST_SCOPE)
 
-#    def tearDown(self):
-        #resolver.scope_map = {resolver.GLOBAL_SCOPE:dict()}
+    def tearDown(self):
+        resolver.scope_map = {resolver.GLOBAL_SCOPE.name: dict(),
+                              resolver.REQUEST_SCOPE.name: dict()}
+        resolver.global_resolver.factories = dict()
 
     def testName(self):
-
         res = resolver.Resolver()
         op = res.resolve(SampleOp)
         self.assertEqual("sub", op.operation(), "factory got right instance")
@@ -59,7 +74,9 @@ class Test(unittest.TestCase):
     def test_delegation(self):
         params = dict()
         headers = dict()
-        request_resolver = resolver.RequestResolver(params,headers)
+        request_resolver = resolver.RequestResolver(params, headers)
+        op = request_resolver.resolve(RequestObject)
+        self.assertEqual("sub", op.do_op(), "factory got right instance")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
